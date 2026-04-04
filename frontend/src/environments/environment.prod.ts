63c78202183c6f2cf937e27790fed3c91f5bc3d0
magicmirror23 <raavikranthikumar23@gmail.com>
2026-04-05 00:41:38 +0530

    mark8

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: {
-    prediction: 'https://stocktrader-prediction-irpt.onrender.com/api/v1',
-    admin: 'https://stocktrader-admin-irpt.onrender.com/api/v1',
-    trading: 'https://stocktrader-trading-irpt.onrender.com/api/v1',
-    marketData: 'https://stocktrader-market-data-irpt.onrender.com/api/v1',
-    gateway: 'https://stocktrader-gateway-irpt.onrender.com/api/v1',
-  },
+  apiBaseUrl: 'https://stocktrader-gateway-irpt.onrender.com/api/v1',
   wsBaseUrl: 'wss://stocktrader-market-data-irpt.onrender.com/api/v1',
   enableMocks: false,
   enableDebugTools: false,
